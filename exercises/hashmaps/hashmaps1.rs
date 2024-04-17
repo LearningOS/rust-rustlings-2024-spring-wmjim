0bd2750b1b7745f5e82a9a0d1c2c52a3856ab0cf
meng.wang <meng.w1016@outlook.com>
2024-04-17 21:32:14 +0800

    complete the hashmap part of the question

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -11,17 +11,21 @@
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();// TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
+    basket.insert(String::from("banana"), 6);
+    basket.entry(String::from("apple")).or_insert(8);
+    basket.entry(String::from("mango")).or_insert(9);
+    basket.entry(String::from("fig")).or_insert(9);
+    basket.entry(String::from("grapes")).or_insert(9);
 
     basket
 }
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,21 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut s = input.to_string();
+    s.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    (input.to_string() + " world!").to_string()
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons").to_string()
 }
 
 #[cfg(test)]
